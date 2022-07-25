4edf37a5c843919fb152b610896d4da651ae57a3
thomas jerez <tjerez@outlook.com>
2022-07-25 14:54:24 -0300

    agregue el ejercicio1

---
+++ js/ejercicio1Tp4.js
@@ -0,0 +1,26 @@
+let auto = {
+    color: [" rojo","negro","blanco"],
+    marca: " Mercedes",
+    modelo: " AMG clase C",
+    motor: false,
+encenderAuto (){
+    this.motor = true
+},
+
+apagarAuto (){
+    this.motor = false
+}
+
+}
+
+
+
+document.write("<br> Color" + auto.color);
+document.write("<br> Marca" + auto.marca);
+document.write("<br> Modelo" + auto.modelo);
+
+auto.encenderAuto();
+document.write("<br> El motor del auto esta" + ((auto.motor)? " Encendido":" Apagado"));
+
+auto.apagarAuto();
+document.write("<br> El motor del auto esta" + ((auto.motor)? " Encendido":" Apagado"));
